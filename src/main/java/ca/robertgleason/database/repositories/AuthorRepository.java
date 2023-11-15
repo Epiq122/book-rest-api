9c7e5bafae6a770353b4040a8ac6188d4988a5b5
Robert Gleason <l2ob122@gmail.com>
2023-11-14 21:46:36 -0800

    setup done

---
+++ src/main/java/ca/robertgleason/database/repositories/AuthorRepository.java
@@ -0,0 +1,16 @@
+package ca.robertgleason.database.repositories;
+
+import ca.robertgleason.database.entities.Author;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Iterable<Author> findByAgeLessThan(int age);
+
+
+    @Query("select a from Author a where a.age > ?1")
+    Iterable<Author> findAuthorsWithAgeGreateThan(int age);
+}
---
+++ src/test/java/ca/robertgleason/database/TestDataUtil.java
@@ -0,0 +1,58 @@
+package ca.robertgleason.database;
+
+import ca.robertgleason.database.entities.Author;
+import ca.robertgleason.database.entities.Book;
+
+public final class TestDataUtil {
+    private TestDataUtil() {
+    }
+
+    public static Author createTestAuthorA() {
+        return Author.builder()
+                .id(1L)
+                .name("Hershal Grove")
+                .age(66)
+                .build();
+    }
+
+    public static Author createTestAuthorB() {
+        return Author.builder()
+                .id(2L)
+                .name("Jerry Leary")
+                .age(16)
+                .build();
+    }
+
+    public static Author createTestAuthorC() {
+        return Author.builder()
+                .id(3L)
+                .name("Merganzer")
+                .age(34)
+                .build();
+    }
+
+
+    public static Book createTestBookA(final Author author) {
+        return Book.builder()
+                .isbn("666-666-6-66-11-2")
+                .title("HarshGallows")
+                .author(author)
+                .build();
+    }
+
+    public static Book createTestBookB(final Author author) {
+        return Book.builder()
+                .isbn("9-116-6-66-11-2")
+                .title("Jumbo Joe")
+                .author(author)
+                .build();
+    }
+
+    public static Book createTestBookC(final Author author) {
+        return Book.builder()
+                .isbn("622-1566-1-36-13-2")
+                .title("Jelously Orchestrate")
+                .author(author)
+                .build();
+    }
+}
