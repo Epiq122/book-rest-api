dba5dd6767d2415157ec7115ed837033a8d5ddbd
Robert Gleason <l2ob122@gmail.com>
2023-11-15 19:42:42 -0800

    created 2 integration tests for getting an author

--- src/main/java/ca/robertgleason/database/controllers/AuthorController.java
+++ src/main/java/ca/robertgleason/database/controllers/AuthorController.java
@@ -5,6 +5,8 @@ import ca.robertgleason.database.domain.entities.Author;
 import ca.robertgleason.database.domain.entities.dto.AuthorDto;
 import ca.robertgleason.database.mappers.Mapper;
 import ca.robertgleason.database.services.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +24,10 @@ public class AuthorController {
 
 
     @PostMapping("/authors")
-    public AuthorDto creatAuthor(@RequestBody AuthorDto authorDto) {
+    public ResponseEntity<AuthorDto> creatAuthor(@RequestBody AuthorDto authorDto) {
         Author author = authorMapper.mapFrom(authorDto);
         Author savedAuthor = authorService.createAuthor(author);
-        return authorMapper.mapTo(savedAuthor);
+        return new ResponseEntity<>(authorMapper.mapTo(savedAuthor), HttpStatus.CREATED);
     }
 
 }
